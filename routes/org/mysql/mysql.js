154d91511f397d21397a82649dbb4a6269b5e6f9
jrs6ttm <jrs6ttm@163.com>
2018-06-05 21:08:12 +0800

    mysql连接池处理

--- routes/org/mysql/mysql.js
+++ routes/org/mysql/mysql.js
@@ -5,7 +5,7 @@ var mysql = require('mysql');
 var env = global.orgENV;
 
 //console.log('mysql ENV:', env, config[env]['mysql']['url']);
-
+/*
 var connection = mysql.createConnection({
 	  host     :  config[env]['mysql']['url'],
 	  user     :  config[env]['mysql']['user'],
@@ -33,7 +33,53 @@ connection.toSQL_noback =  function toSQL_noback(sqlstr){
 		if(err) console.log("MySql :toSQL_noback err:" + err);
    });
 }
+*/
 
+var pool = mysql.createPool({
+	host     :  config[env]['mysql']['url'],
+	user     :  config[env]['mysql']['user'],
+	password :  config[env]['mysql']['password'],
+	database :  config[env]['mysql']['database'],
+	port     : 3306
+});
+
+var query = {
+	toSQL : function toSQL(sqlstr, callback){
+		pool.getConnection(function(err, conn){
+			if(err){
+				callback(err, null);
+			}else{
+				console.log("connect pool created.");
+				conn.query(sqlstr, [], function(err,results){
+					if(err) console.log("MySql :toSQL err:" + err);
+					//释放连接
+					conn.release();
+					//事件驱动回调
+					callback(err,results);
+				});
+			}
+		});
+	},
+
+	toSQL_noback :  function toSQL_noback(sqlstr){
+		connection.query(sqlstr, function(err, doc) {
+			if(err) console.log("MySql :toSQL_noback err:" + err);
+		});
+		pool.getConnection(function(err, conn){
+			if(err){
+				console.log("MySql :toSQL_noback err:" + err);
+			}else{
+				conn.query(sqlstr, [], function(err,results){
+					if(err) console.log("MySql :toSQL_noback err:" + err);
+					//释放连接
+					conn.release();
+				});
+			}
+		});
+	}
+};
+
+module.exports = query;
 
 
 
--- routes/org/mysql/mysql2.js
+++ routes/org/mysql/mysql2.js
@@ -7,6 +7,7 @@ console.log('mysql ENV:', env, config[env]['mysql']['url']);
 
 console.log(config[env]);
 
+/*
 var connection = mysql.createConnection({
 	  host     :  config[env]['OC_mysql']['url'],
 	  user     :  config[env]['OC_mysql']['user'],
@@ -34,7 +35,53 @@ connection.toSQL_noback =  function toSQL_noback(sqlstr){
 		if(err) console.log("MySql :toSQL_noback err:" + err);
    });
 }
+*/
 
+var pool = mysql.createPool({
+	host     :  config[env]['OC_mysql']['url'],
+	user     :  config[env]['OC_mysql']['user'],
+	password :  config[env]['OC_mysql']['password'],
+	database :  config[env]['OC_mysql']['database'],
+	port     : 3306
+});
+
+var query = {
+	toSQL : function toSQL(sqlstr, callback){
+		pool.getConnection(function(err, conn){
+			if(err){
+				callback(err, null);
+			}else{
+				console.log("connect pool created.");
+				conn.query(sqlstr, [], function(err,results){
+					if(err) console.log("MySql :toSQL err:" + err);
+					//释放连接
+					conn.release();
+					//事件驱动回调
+					callback(err,results);
+				});
+			}
+		});
+	},
+
+	toSQL_noback :  function toSQL_noback(sqlstr){
+		connection.query(sqlstr, function(err, doc) {
+			if(err) console.log("MySql :toSQL_noback err:" + err);
+		});
+		pool.getConnection(function(err, conn){
+			if(err){
+				console.log("MySql :toSQL_noback err:" + err);
+			}else{
+				conn.query(sqlstr, [], function(err,results){
+					if(err) console.log("MySql :toSQL_noback err:" + err);
+					//释放连接
+					conn.release();
+				});
+			}
+		});
+	}
+};
+
+module.exports = query;
 
 
 
